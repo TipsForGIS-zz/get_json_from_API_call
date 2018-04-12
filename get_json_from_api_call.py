d05b889e107948b57702e6ce6202ee0bdfa645fd
Ahmad Aburizaiza - Tips for GIS <tipsforgis@gmail.com>
2018-04-12 08:08:38 -0400

    Update get_json_from_api_call.py

--- get_json_from_api_call.py
+++ get_json_from_api_call.py
@@ -1,7 +1,7 @@
 import requests
 
 # build the API URL
-api_url = 'http://someAPIaddress/?lon='+str(-112.57027151831147)+'&lat='+str(35.41778464478152)
+api_url = 'http://someAPIaddress?lon='+str(-112.57027151831147)+'&lat='+str(35.41778464478152)
 # execute the API call and store the returned response
 response = requests.get(api_url)
 # print the response as JSON
